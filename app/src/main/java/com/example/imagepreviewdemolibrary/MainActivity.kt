ce411d9a63861ad1174c04961f0164413cd2e3c2
Sushant1290 <sushant.kumarchhotaray@iserveu.co.in>
2024-04-01 13:35:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/imagepreviewdemolibrary/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.imagepreviewdemolibrary
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.image_preview.ImagePreview
+import com.example.imagepreviewdemolibrary.ui.theme.ImagePreviewDemoLibraryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ImagePreviewDemoLibraryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ImagePreview(image = painterResource(id = R.drawable.img))
+                }
+            }
+        }
+    }
+}
---
+++ image-preview/src/main/java/com/example/image_preview/ImagePreview.kt
@@ -0,0 +1,32 @@
+package com.example.image_preview
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ImagePreview(
+    image: Painter,
+    contentDescription: String = ""
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = image,
+            contentDescription = contentDescription,
+            modifier = Modifier
+                .clip(CircleShape)
+                .size(20.dp)
+        )
+    }
+}
